2bbff3a9b36d34f50b358675ab3fb504bfeddf9c
Gustavo Floriano Fernandes <107115233+GustavoFloriano1@users.noreply.github.com>
2022-06-08 09:11:07 -0300

    Add files via upload

---
+++ exer3.java
@@ -0,0 +1,23 @@
+package exercicio3;
+
+import java.util.Scanner;
+
+public class exer3 {
+    public static void main(String[] args) {
+        float a, b, c;
+        double d;
+        Scanner i = new Scanner(System.in);
+
+        a = 3 - 2 - 1 + 2 + 1 + 3;
+        b = (2 * 3) - (4 * 5);
+        c = 2 + 6 - ((3 / 7) * 9);
+        d = 3 % 4;
+
+        System.out.println("Resultado da questão a é de: " + a);
+        System.out.println("Resultado da questão b é de: " + b);
+        System.out.println("Resultado da questão c é de: " + c);
+        System.out.println("Resultado da questão d é de: " + d);
+
+    }
+
+}
